5d891b9891c2fb01f62a4aaaa8282ae039a84150
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-02-14 13:25:44 -0300

    Initial Commit

---
+++ TimesTableGame/TimesTableGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTableGameApp.swift
+//  TimesTableGame
+//
+//  Created by Bruno Oliveira on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TimesTableGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
