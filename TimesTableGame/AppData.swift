f7a952ed4e52ee2c2a75fcc69f4933fc2c98565d
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-06-18 05:48:05 -0300

    Include App Version on UI to Ident. Alpha Version

---
+++ TimesTableGame/AppData.swift
@@ -0,0 +1,14 @@
+//
+//  AppData.swift
+//  TimesTableGame
+//
+//  Created by Bruno Oliveira on 18/06/24.
+//
+
+import Foundation
+
+struct AppData {
+    static let version = "v0.1.0a"
+    static let developerURL = "https://www.includesystems.com"
+    static let developerName = "Bruno Ricardo de Oliveira"
+}
